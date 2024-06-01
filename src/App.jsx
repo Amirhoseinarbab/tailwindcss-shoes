8d55807e8ec476e117b2cb5e0d88e0077685649e
aMIrMXC <amirmxc@gmail.com>
2024-06-02 02:35:08 +0330

    fix3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import { Home } from "./pages/Home";
-import {Purchases} from "./pages/Purchases";
+import { Purchases } from "./pages/Purchases";
 import { useEffect, useState } from "react";
 import { SHOE_LIST_FA } from "./constant";
 import UserData from "./pages/UserData";
@@ -17,8 +17,7 @@ export function App() {
       window.document.documentElement.classList.add("dark");
     }
   }, []);
-  
-  
+
   const toggleDarkMode = () => {
     window.document.documentElement.classList.toggle("dark");
     localStorage.setItem(
@@ -26,7 +25,7 @@ export function App() {
       window.document.documentElement.classList.contains("dark"),
     );
   };
-  
+
   const removeFromCart = (productId) => {
     console.log("***", productId);
     const updatedCartItems = [...cartItems];
@@ -48,53 +47,71 @@ export function App() {
       } else {
         updatedCartItems.push({ product, qty, size });
       }
-  
+
       setCartItems(updatedCartItems);
     }
-};
+  };
 
+  //   return (
+  //     <div className="animate-fadeIn p-10 dark:bg-night xl:px-24 noto-kufi-arabic">
+  //       <Nav
+  //         onClickShoppingBtn={() => setIsSidebarOpen(true)}
+  //       />
+  //       <ShoeDetail shoe={currentShoe} onClickAdd={addToCart} />
+  //       <NewArrivalsSection items={SHOE_LIST_FA} onClickCard={setCurrentShoe} />
+  //       <Sidebar
+  //         isOpen={isSidebarOpen}
+  //         onClickClose={() => setIsSidebarOpen(false)}
+  //       >
+  //         <Cart cartItems={cartItems} onClickTrash={removeFromCart} />
+  //       </Sidebar>
+  //       <div className=" fixed bottom-4 right-4">
+  //         <button
+  //           onClick={toggleDarkMode}
+  //           className="rounded-full bg-night-50 px-4 py-2 text-white shadow-lg dark:bg-white dark:text-night"
+  //         >
+  //           <BiSun className="hidden dark:block" />
+  //           <BiMoon className="dark:hidden" />
+  //         </button>
+  //       </div>
+  //     </div>
 
-//   return (
-//     <div className="animate-fadeIn p-10 dark:bg-night xl:px-24 noto-kufi-arabic">
-//       <Nav
-//         onClickShoppingBtn={() => setIsSidebarOpen(true)}
-//       />
-//       <ShoeDetail shoe={currentShoe} onClickAdd={addToCart} />
-//       <NewArrivalsSection items={SHOE_LIST_FA} onClickCard={setCurrentShoe} />
-//       <Sidebar
-//         isOpen={isSidebarOpen}
-//         onClickClose={() => setIsSidebarOpen(false)}
-//       >
-//         <Cart cartItems={cartItems} onClickTrash={removeFromCart} />
-//       </Sidebar>
-//       <div className=" fixed bottom-4 right-4">
-//         <button
-//           onClick={toggleDarkMode}
-//           className="rounded-full bg-night-50 px-4 py-2 text-white shadow-lg dark:bg-white dark:text-night"
-//         >
-//           <BiSun className="hidden dark:block" />
-//           <BiMoon className="dark:hidden" />
-//         </button>
-//       </div>
-//     </div>
+  // );
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* <Route index element={<Test />} /> */}
 
-
-// );
-return(
-  
-  
-  <Routes>
-      <Route path="/" element={<Layout />} >
-
-      {/* <Route index element={<Test />} /> */}
-
-      <Route index element={<Home currentShoe={currentShoe} setCurrentShoe={setCurrentShoe} cartItems={cartItems} setCartItems={setCartItems} addToCart={addToCart} toggleDarkMode={toggleDarkMode} onClickTrash={removeFromCart}/>} />
-      <Route path="/purchases" element={<Purchases currentShoe={currentShoe} setCurrentShoe={setCurrentShoe} cartItems={cartItems} setCartItems={setCartItems} addToCart={addToCart}  onClickTrash={removeFromCart}/>} />
-    <Route path="/user-informations" element={<UserData />} />
-      <Route path="/success" element={<Success />} />
-    </Route>
+        <Route
+          index
+          element={
+            <Home
+              currentShoe={currentShoe}
+              setCurrentShoe={setCurrentShoe}
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+              addToCart={addToCart}
+              toggleDarkMode={toggleDarkMode}
+              onClickTrash={removeFromCart}
+            />
+          }
+        />
+        <Route
+          path="/purchases"
+          element={
+            <Purchases
+              currentShoe={currentShoe}
+              setCurrentShoe={setCurrentShoe}
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+              addToCart={addToCart}
+              onClickTrash={removeFromCart}
+            />
+          }
+        />
+        <Route path="/user-informations" element={<UserData />} />
+        <Route path="/success" element={<Success />} />
+      </Route>
     </Routes>
-      
-    )
-  }
-  
+  );
+}
